10315080d1bd71eb075182c29f234c30a4165a8b
Titus Wormer <tituswormer@gmail.com>
2023-05-30 17:44:56 +0200

    Fix xo

--- test-types.d.ts
+++ test-types.d.ts
@@ -1,18 +1,22 @@
-import {Literal} from 'mdast'
+import type {Literal} from 'mdast'
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface Toml extends Literal {
   type: 'toml'
 }
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface Json extends Literal {
   type: 'json'
 }
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface Custom extends Literal {
   type: 'custom'
 }
 
 declare module 'mdast' {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface FrontmatterContentMap {
     toml: Toml
     custom: Custom
